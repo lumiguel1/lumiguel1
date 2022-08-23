6078812c417bf91bc1016635bf8d3b28f3c05f6a
LuMiguel1 <luizmiguelreis2000@gmail.com>
2022-08-23 16:05:46 -0300

    Created pofile modal

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-console.log("Oi");
-window.location.href='#tecno';
+function onCheckBox(x) {
+   var a = document.querySelector("#checc").checked;
+   console.log(a);
+   if(a == false) {
+        console.log("desativado")
+        document.getElementById("navPerfil").setAttribute("class", "profile-panel");
+   } else {
+        document.getElementById("navPerfil").setAttribute("class", "nav-active");
+   }
+}
+
+function closeNav(x) {
+    let className = document.querySelector("#navPerfil").className;
+    if(className = "nav-active"){
+        let c = document.querySelector("#checc").checked = false;
+        onCheckBox();
+    }
+}
